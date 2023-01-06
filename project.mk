79815f78e7b589564d926e861b0ec9c80021aaea
xzqiaochu <xzzhangqiaochu@163.com>
2023-01-06 17:44:14 +0800

    first commit

---
+++ project.mk
@@ -0,0 +1,32 @@
+# This file can be used to set build configuration
+# variables.  These variables are defined in a file called 
+# "Makefile" that is located next to this one.
+
+# For instructions on how to use this system, see
+# https://github.com/Analog-Devices-MSDK/VSCode-Maxim/tree/develop#build-configuration
+
+#MXC_OPTIMIZE_CFLAGS = -Og
+# ^ For example, you can uncomment this line to 
+# optimize the project for debugging
+
+# **********************************************************
+
+# Add your config here!
+
+$(info Note: This project is designed and tested for the OV7692 only.)
+override CAMERA=OV7692
+
+# Set a higher optimization level.  The increased performance
+# is required for the CameraIF DMA code to work within the
+# timing requirements of the Parallel Camera Interface.
+MXC_OPTIMIZE_CFLAGS = -O2
+
+ifeq "$(BOARD)" "EvKit_V1"
+VPATH += TFT/evkit/resources
+endif
+ifeq "$(BOARD)" "FTHR_RevA"
+VPATH += TFT/fthr
+endif
+
+IPATH += TFT/evkit/resources
+
